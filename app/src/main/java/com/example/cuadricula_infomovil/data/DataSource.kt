26650f6421902beeb3d89e6195f6e6b8171c9c02
magilssa <magilssa@unirioja.es>
2024-02-20 18:49:44 +0100

    Initial commit

---
+++ app/src/main/java/com/example/cuadricula_infomovil/data/DataSource.kt
@@ -0,0 +1,23 @@
+package com.example.cuadricula_infomovil.data
+
+import com.example.cuadricula_infomovil.R
+import com.example.cuadricula_infomovil.model.Topic
+
+object DataSource {
+    val topics = listOf(
+        Topic(R.string.architecture, 58, R.drawable.unirioja),
+        Topic(R.string.crafts, 121, R.drawable.unirioja),
+        Topic(R.string.business, 78, R.drawable.unirioja),
+        Topic(R.string.culinary, 118, R.drawable.unirioja),
+        Topic(R.string.design, 423, R.drawable.unirioja),
+        Topic(R.string.fashion, 92, R.drawable.unirioja),
+        Topic(R.string.film, 165, R.drawable.unirioja),
+        Topic(R.string.gaming, 164, R.drawable.unirioja),
+        Topic(R.string.drawing, 326, R.drawable.unirioja),
+        Topic(R.string.lifestyle, 305, R.drawable.unirioja),
+        Topic(R.string.music, 212, R.drawable.unirioja),
+        Topic(R.string.painting, 172, R.drawable.unirioja),
+        Topic(R.string.photography, 321, R.drawable.unirioja),
+        Topic(R.string.tech, 118, R.drawable.unirioja)
+    )
+}
